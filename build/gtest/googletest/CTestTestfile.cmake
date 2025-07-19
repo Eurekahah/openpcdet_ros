2de6c456e71c43129e676315cda4184cae2b8e29
Eurekahah <a984036428@gmail.com>
2025-07-19 11:34:36 +0800

    取消对build文件的跟踪

--- build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/eureka/code/OpenPCDet_ws/src/OpenPCDet_ros/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
